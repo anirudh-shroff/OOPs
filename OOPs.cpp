b55f9cb3dbe8e0fc607f3b71c831b60a23eb798d
anirudh-shroff <shroffstar8@gmail.com>
2025-03-26 10:54:23 +0530

    object oriented programming first lecture example in C++

--- OOPs.cpp
+++ OOPs.cpp
@@ -2,9 +2,37 @@
 
 using namespace std;
 
+class Student{
+
+    public:
+    int age;
+    string name;
+    string coarse;
+    int grid;
+    float percentage;
+
+};
+
 int main(){
 
+    Student student1;
 
-    
+    cout<< "Please enter student name : "<<endl;
+    cin>>student1.name;
+
+    cout<< "Please enter student age : "<<endl;
+    cin>>student1.age;
+
+    cout<< "Please enter coarse name : "<<endl;
+    cin>>student1.coarse;
+
+    cout<<"Please enter G.R.I.D. : "<<endl;
+    cin>>student1.grid;
+
+
+    cin>>student1.percentage;
+
+    // cout<<student1.age;
+
     return 0;
 }
